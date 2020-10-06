abfa508335242b7edc488fed99e0c80e9a5b685d
LIHUI <110496364@qq.com>
2020-10-06 20:11:29 +0800

    Updated modbusServer.go adding dynamic simulated data.

--- modbusServer.go
+++ modbusServer.go
@@ -24,6 +24,7 @@ type tomlConfig struct {
 	MbTcp   modbusTCP `toml:"modbusTCP"`
 	MbRtu   modbusRTU `toml:"modbusRTU"`
 	MbReg   registers `toml:"registers"`
+	MbSim   simulated `toml:"simulated"`
 }
 
 type enable struct {
@@ -53,6 +54,13 @@ type registers struct {
 	HoldingRegisters  [][2]uint32  `toml:"holdingRegisters"`
 }
 
+type simulated struct {
+	DataDiscreteInputs    [][2][]uint32  `toml:"dataDiscreteInputs"`
+	DataCoils             [][2][]uint32  `toml:"dataCoils"`
+	DataInputRegisters    [][2][]uint32  `toml:"dataInputRegisters"`
+	DataHoldingRegisters  [][2][]uint32  `toml:"dataHoldingRegisters"`
+}
+
 var applName string
 var config  tomlConfig
 var server *mbserver.Server
@@ -61,6 +69,18 @@ var buf bytes.Buffer
 var fileSet string
 var fileLog string
 
+var mapDiscreteInputs    map[uint32][]uint32
+var mapCoils             map[uint32][]uint32
+var mapInputRegisters    map[uint32][]uint32
+var mapHoldingRegisters  map[uint32][]uint32
+var lenDiscreteInputs    map[uint32]uint32
+var lenCoils             map[uint32]uint32
+var lenInputRegisters    map[uint32]uint32
+var lenHoldingRegisters  map[uint32]uint32
+var idxDiscreteInputs    map[uint32]uint32
+var idxCoils             map[uint32]uint32
+var idxInputRegisters    map[uint32]uint32
+var idxHoldingRegisters  map[uint32]uint32
 
 func init() {
 	strApp := path.Base(os.Args[0])
@@ -102,6 +122,7 @@ func main() {
 	}
 
 	for {
+		updateModbusServer(server)
 		time.Sleep(time.Second)
 	}
 }
@@ -143,6 +164,42 @@ func executeSettings() {
 			buf.WriteString(fmt.Sprintln("%v", err))
 		}
 	}
+
+	mapDiscreteInputs = make(map[uint32][]uint32)
+	lenDiscreteInputs = make(map[uint32]uint32)
+	idxDiscreteInputs = make(map[uint32]uint32)
+	for _, v := range config.MbSim.DataDiscreteInputs {
+		mapDiscreteInputs[v[0][0]] = v[1]
+		lenDiscreteInputs[v[0][0]] = uint32(len(v[1]))
+		idxDiscreteInputs[v[0][0]] = 0
+	}
+
+	mapCoils = make(map[uint32][]uint32)
+	lenCoils = make(map[uint32]uint32)
+	idxCoils = make(map[uint32]uint32)
+	for _, v := range config.MbSim.DataCoils {
+		mapCoils[v[0][0]] = v[1]
+		lenCoils[v[0][0]] = uint32(len(v[1]))
+		idxCoils[v[0][0]] = 0
+	}
+
+	mapInputRegisters = make(map[uint32][]uint32)
+	lenInputRegisters = make(map[uint32]uint32)
+	idxInputRegisters = make(map[uint32]uint32)
+	for _, v := range config.MbSim.DataInputRegisters {
+		mapInputRegisters[v[0][0]] = v[1]
+		lenInputRegisters[v[0][0]] = uint32(len(v[1]))
+		idxInputRegisters[v[0][0]] = 0
+	}
+
+	mapHoldingRegisters = make(map[uint32][]uint32)
+	lenHoldingRegisters = make(map[uint32]uint32)
+	idxHoldingRegisters = make(map[uint32]uint32)
+	for _, v := range config.MbSim.DataHoldingRegisters {
+		mapHoldingRegisters[v[0][0]] = v[1]
+		lenHoldingRegisters[v[0][0]] = uint32(len(v[1]))
+		idxHoldingRegisters[v[0][0]] = 0
+	}
 }
 
 func initModbusServer(s *mbserver.Server) {
@@ -160,6 +217,44 @@ func initModbusServer(s *mbserver.Server) {
 
 	for _, v := range config.MbReg.HoldingRegisters {
 		s.HoldingRegisters[v[0]] = uint16(v[1])
+	}
+}
+
+func updateModbusServer(s *mbserver.Server) {
+	for k, v := range idxDiscreteInputs{
+		s.DiscreteInputs[k] = byte(mapDiscreteInputs[k][v])
+	}
+	for k, v := range idxDiscreteInputs {
+		tmp := v + 1
+		tmp %= lenDiscreteInputs[k]
+		idxDiscreteInputs[k] = tmp
+	}
+
+	for k, v := range idxCoils {
+		s.Coils[k] = byte(mapCoils[k][v])
+	}
+	for k, v := range idxCoils {
+		tmp := v + 1
+		tmp %= lenCoils[k]
+		idxCoils[k] = tmp
+	}
+
+	for k, v := range idxInputRegisters {
+		s.InputRegisters[k] = uint16(mapInputRegisters[k][v])
+	}
+	for k, v := range idxInputRegisters {
+		tmp := v + 1
+		tmp %= lenInputRegisters[k]
+		idxInputRegisters[k] = tmp
+	}
+
+	for k, v := range idxHoldingRegisters {
+		s.HoldingRegisters[k] = uint16(mapHoldingRegisters[k][v])
+	}
+	for k, v := range idxHoldingRegisters {
+		tmp := v + 1
+		tmp %= lenHoldingRegisters[k]
+		idxHoldingRegisters[k] = tmp
 	}
 }
 
@@ -188,4 +283,24 @@ const strDefaultSettings =
   coils            = [[0,    60], [1,    10], [2,    20], [3,    30]]
   inputRegisters   = [[0,   600], [1,   100], [2,   200], [3,   300]]
   holdingRegisters = [[0, 60000], [1, 10000], [2, 20000], [3, 30000]]
+
+[simulated]
+  dataDiscreteInputs   = [[[100], [0,1]],
+                          [[101], [0,1,1,0]],
+                          [[102], [0,1,1,0,0,0]],
+                          [[103], [0,1,1,0,1,0,0,1,1,1,1]]]
+
+  dataCoils            = [[[200], [0,0,0,0,0,0]],
+                          [[201], [0,1,1,0]],
+                          [[202], [0,1,1,0,0,0]],
+                          [[203], [0,1,1,0,1,0,0,1,1,1]]]
+  dataInputRegisters   = [[[300], [ 0,1]],
+                          [[301], [10,11,12,13]],
+                          [[302], [20,21,22,23,24,25]],
+                          [[303], [30,31,32,33,34,35,36,37,38,39]]]
+
+  dataHoldingRegisters = [[[400], [ 0,99]],
+                          [[401], [10,11,12,13]],
+                          [[402], [20,21,22,23,24,25]],
+                          [[403], [30,31,32,33,34,35,36,37,38,39]]]
 `
